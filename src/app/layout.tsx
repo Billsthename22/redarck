d0255b59ed9de6121ae123247d8143d71b613838
John chukwu <ifeanyichukwujohn23@gmail.com>
2025-07-23 23:31:46 +0100

    fix6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,8 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
-import { Geist, Geist_Mono } from "next/font/google";
 import { CartProvider } from "@/app/Context/cartcontext";
-import CartSidebar from "@/app/components/CartSidebar"; //  Import sidebar
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import CartSidebar from "@/app/components/CartSidebar"; // Import sidebar
 
 export const metadata: Metadata = {
   title: "Redarck",
@@ -27,21 +15,29 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Koulen&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Konkhmer+Sleokchher&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Kristi&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Leckerli+One&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Koulen&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Konkhmer+Sleokchher&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kristi&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Leckerli+One&display=swap"
+          rel="stylesheet"
+        />
       </head>
-
-      <body
-        className={`
-          ${geistSans.variable}
-          ${geistMono.variable}
-          antialiased
-        `}
-      >
-        
+      <body className="antialiased">
         <CartProvider>
           {children}
           <CartSidebar /> {/* ✅ Always rendered, hidden until toggled */}
